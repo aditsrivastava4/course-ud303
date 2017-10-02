6d76bb4f209362a0893afe0195748d0e036521cf
Adit Srivastava <aditsriv@live.com>
2017-10-03 03:17:53 +0530

    Completed Exercise: Messageboard, Part Two

--- Lesson-2/4_MessageboardPartTwo/MessageboardPartTwo.py
+++ Lesson-2/4_MessageboardPartTwo/MessageboardPartTwo.py
@@ -17,6 +17,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
 
+data = '''
+<!DOCTYPE html>
+  <title>Message Board</title>
+  <form method="POST" action="http://localhost:8008/">
+    <textarea name="message"></textarea>
+    <br>
+    <button type="submit">Post it!</button>
+  </form>
+'''
+
 
 class MessageHandler(BaseHTTPRequestHandler):
     def do_POST(self):
@@ -35,7 +45,14 @@ class MessageHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(message.encode())
 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(data.encode())
+
+
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    server_address = ('', 8008)
     httpd = HTTPServer(server_address, MessageHandler)
     httpd.serve_forever()
